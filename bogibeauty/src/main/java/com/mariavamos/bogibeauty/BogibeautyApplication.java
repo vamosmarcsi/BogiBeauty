7422aa047098451f89d65285fc6d7114c75609a8
vamosmarcsi <marcsivamos0308@gmail.com>
2024-02-09 21:38:33 +0100

    1 - project initialization

---
+++ bogibeauty/src/main/java/com/mariavamos/bogibeauty/BogibeautyApplication.java
@@ -0,0 +1,13 @@
+package com.mariavamos.bogibeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BogibeautyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BogibeautyApplication.class, args);
+	}
+
+}
---
+++ bogibeauty/src/test/java/com/mariavamos/bogibeauty/BogibeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariavamos.bogibeauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BogibeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
